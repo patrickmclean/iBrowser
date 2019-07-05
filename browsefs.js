a3f2069ba09db01223b6b0e52a93d1d84b509a00
patrickmclean <patrickmclean@yahoo.com>
2019-07-05 15:21:25 +0200

    More complete file information

--- browsefs.js
+++ browsefs.js
@@ -1,16 +1,25 @@
 fs = require('fs');
 path = require('path');
+uuid = require('uuid');
 
 module.exports = {
     iterateDirectoryImages: function(dir,done) {
         var results = [];
         that = this;
         fs.readdir(dir, function(err, list) {
+            
             if (err) return done(err);
             var pending = list.length;
             if (!pending) return done(null, results);
             list.forEach(function(file) {
                 file = path.resolve(dir, file);
+                var image = {
+                    filename : path.basename(file),
+                    folder : path.relative(that.currentDirectory,path.dirname(file)),
+                    uuid : uuid(),
+                    date : new Date()
+                }
+
                 fs.stat(file, function(err, stat) {
                     if (stat && stat.isDirectory()) {
                         that.iterateDirectoryImages(file, function(err, res) {
@@ -19,7 +28,8 @@ module.exports = {
                         })
                     } else {
                         if ((path.extname(file) == ".jpg") || (path.extname(file) == ".JPG") ) {
-                            results.push(file);
+                            image.date = stat.birthtime;
+                            results.push(image);
                         }
                         if (!--pending) done(null, results);
                     }
@@ -29,3 +39,4 @@ module.exports = {
     },
     currentDirectory: String
 }
+
--- main.js
+++ main.js
@@ -3,12 +3,11 @@ ddb = require('./awsdynamodb');
 
 bfs.currentDirectory = './';
 
-/*
 bfs.iterateDirectoryImages(bfs.currentDirectory, function(err, results) {
     if (err) throw err;
     console.log(results);
   });
-*/
+
 
 /*ddb.addTestData();
 ddb.addTestData();*/
